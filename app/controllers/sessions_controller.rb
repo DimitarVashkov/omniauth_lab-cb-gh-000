b5ec61c70fde3ad0653fe262b7b3cc2454650caa
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-16 14:04:38 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,15 @@ class SessionsController < ApplicationController
       u.email = auth['info']['email']
       u.image = auth['info']['image']
     end
- 
+
     session[:user_id] = @user.id
- 
+
     render 'welcome/home'
   end
- 
+
   private
- 
+
   def auth
     request.env['omniauth.auth']
   end
-
 end
